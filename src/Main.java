03d4a4ce0611cbfed8979819efb83bbf920d889f
Rinat Agliev <rinat.naisovich@mail.ru>
2024-04-16 21:43:55 +0300

    check file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("длина текста " + text.length());
+        int i = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу и нажмите <Enter>:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory == true) {
+                System.out.println("Это папка");
+                continue;
+            }
+            if (fileExists == false) {
+                System.out.println("Файл не существует");
+                continue;
+            }
+            if (fileExists == true) {
+                System.out.println("Путь указан верно");
+                i++;
+                System.out.println("Это файл номер: " + i);
+            }
+        }
     }
 }
